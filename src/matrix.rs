c09f4729a1703a2281c07185f99b1338cdd3f062
Ivar Stangeby <istangeby@gmail.com>
2024-03-29 19:32:50 +0100

    Test matrix comparison

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Matrix {
     values: Vec<f64>,
     rows: usize,
@@ -18,6 +19,26 @@ impl Matrix {
     }
 }
 
+impl PartialEq for Matrix {
+    fn eq(&self, other: &Self) -> bool {
+        if self.values.len() != other.values.len() {
+            return false;
+        }
+
+        if self.rows != other.rows || self.columns != other.columns {
+            return false;
+        }
+
+        for i in 0..self.values.len() {
+            if (self.values[i] - other.values[i]).abs() > f64::EPSILON {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::matrix::Matrix;
@@ -30,5 +51,18 @@ mod tests {
         assert_eq!(m.get(0, 1), 5.0);
         assert_eq!(m.get(1, 0), 1.0);
         assert_eq!(m.get(1, 1), -2.0);
+    }
+
+    #[test]
+    fn matrix_comparison() {
+        let m1 = Matrix::new(vec![-3.0, 5.0, 1.0, -2.0], 2, 2);
+        let m2 = Matrix::new(vec![-3.0000001, 5.0, 1.0, -2.0], 2, 2);
+        let m3 = Matrix::new(vec![-3.0, 5.0, 1.0, -2.0, 1.0], 3, 2);
+        let m4 = Matrix::new(vec![-3.0, 5.0, 1.0, -2.0, 1.0], 2, 3);
+
+        assert_eq!(m1, m1);
+        assert_ne!(m1, m3);
+        assert_ne!(m1, m2);
+        assert_ne!(m3, m4);
     }
 }
