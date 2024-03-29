50f1b9cbbd43481c06aa3a491eb106e86d900295
Ivar Stangeby <istangeby@gmail.com>
2024-03-29 12:57:28 +0100

    Create a small rust-cannon that shoots a projectile and logs the position until it hits the ground.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,35 @@ mod tuple;
 
 use crate::tuple::Tuple;
 
+struct Environment {
+    gravity: Tuple,
+    wind: Tuple,
+}
+
+struct Projectile {
+    position: Tuple,
+    velocity: Tuple,
+}
+
+fn tick(environment: &Environment, projectile: &Projectile) -> Projectile {
+    Projectile {
+        position: projectile.position + projectile.velocity,
+        velocity: projectile.velocity + environment.gravity + environment.wind,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let e = Environment {
+        gravity: Tuple::vector(0.0, -0.1, 0.0),
+        wind: Tuple::vector(-0.01, 0.0, 0.0),
+    };
+    let mut p = Projectile {
+        position: Tuple::vector(0.0, 1.0, 0.0),
+        velocity: Tuple::vector(1.0, 1.0, 0.0).norm(),
+    };
+
+    while p.position.y > 0.0 {
+        println!("{:?}", p.position);
+        p = tick(&e, &p);
+    }
 }
