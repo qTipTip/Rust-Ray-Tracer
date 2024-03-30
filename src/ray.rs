edcb4e28a667cfcb22de6db46893834a4ed99b6a
Ivar Stangeby <istangeby@gmail.com>
2024-03-30 19:35:37 +0100

    Test position

--- src/ray.rs
+++ src/ray.rs
@@ -12,6 +12,10 @@ impl Ray {
         }
         Self { origin, direction }
     }
+
+    fn position(&self, t: f64) -> Tuple {
+        self.origin + self.direction * t
+    }
 }
 
 #[cfg(test)]
@@ -30,5 +34,20 @@ mod tests {
 
         assert_eq!(r.origin, origin);
         assert_eq!(r.direction, direction);
+    }
+
+    #[test]
+    fn compute_position() {
+        let origin = Tuple::point(2.0, 3.0, 4.0);
+        let direction = Tuple::vector(1.0, 0.0, 0.0);
+        let r = Ray::new(
+            origin,
+            direction,
+        );
+
+        assert_eq!(r.position(0.0), origin);
+        assert_eq!(r.position(1.0), Tuple::point(3.0, 3.0, 4.0));
+        assert_eq!(r.position(-1.0), Tuple::point(1.0, 3.0, 4.0));
+        assert_eq!(r.position(2.5), Tuple::point(4.5, 3.0, 4.0));
     }
 }
