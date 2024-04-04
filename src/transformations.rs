3965205c578bcba9c7846ea4eed604a652646e56
Ivar Stangeby <istangeby@gmail.com>
2024-04-05 00:19:01 +0200

    naming

--- src/transformations.rs
+++ src/transformations.rs
@@ -244,26 +244,26 @@ mod tests {
     #[test]
     fn apply_transformations_in_sequence() {
         let p = Tuple::point(1.0, 0.0, 1.0);
-        let A = rotation_x(PI / 2.0);
-        let B = scaling(5.0, 5.0, 5.0);
-        let C = translation(10.0, 5.0, 7.0);
+        let a = rotation_x(PI / 2.0);
+        let b = scaling(5.0, 5.0, 5.0);
+        let c = translation(10.0, 5.0, 7.0);
 
-        let p2 = A * p;
+        let p2 = a * p;
         assert_eq!(p2, Tuple::point(1.0, -1.0, 0.0));
 
-        let p3 = B * p2;
+        let p3 = b * p2;
         assert_eq!(p3, Tuple::point(5.0, -5.0, 0.0));
 
-        let p4 = C * p3;
+        let p4 = c * p3;
         assert_eq!(p4, Tuple::point(15.0, 0.0, 7.0));
     }
 
     fn apply_transformations_chained() {
         let p = Tuple::point(1.0, 0.0, 1.0);
-        let A = rotation_x(PI / 2.0);
-        let B = scaling(5.0, 5.0, 5.0);
-        let C = translation(10.0, 5.0, 7.0);
+        let a = rotation_x(PI / 2.0);
+        let b = scaling(5.0, 5.0, 5.0);
+        let c = translation(10.0, 5.0, 7.0);
 
-        assert_eq!(C * B * A * p, Tuple::point(15.0, 0.0, 7.0));
+        assert_eq!(c * b * a * p, Tuple::point(15.0, 0.0, 7.0));
     }
 }
